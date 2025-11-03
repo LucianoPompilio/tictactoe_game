12d5c0c02d209795e183bf9fe0473a854234a232
Luciano Pompilio <lucianopompilio123@gmail.com>
2025-11-02 21:54:31 -0300

    new bottons editname

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import Player from './components/Player'
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+   <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Alice" symbol="X" />
+        <Player name="Bob" symbol="O" />
+      </ol>
+      GAME BOARD
+    </div>
+    LOG
+   </main>  
   )
 }
 
